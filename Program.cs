54670098a3f39ccafe5e69388fafbcda309e4047
Detective-Coder <aaronfrankenfield@gmail.com>
2021-04-21 15:46:56 -0400

    finished bank custom types exercise

--- Program.cs
+++ Program.cs
@@ -4,6 +4,18 @@ using System.Linq;
 
 namespace linq
 {
+    public class Customer
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
+    }
+
+    public class Bank
+    {
+      public string BankName;
+      public int NumberOfCustomers;
+    }
     class Program
     {
         static void Main(string[] args)
@@ -51,6 +63,10 @@ namespace linq
             }
 
             // // Build a collection of these numbers sorted in ascending order
+            // List<int> numbers = new List<int>()
+            // {
+            //     15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            // };
 
             var ascendingNumbers = numbers.OrderBy(n => n);
 
@@ -66,6 +82,61 @@ namespace linq
             // };
             Console.WriteLine();
             Console.WriteLine("There are " + numbers.Count + " numbers in the numbers list.");
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+            Console.WriteLine();
+            Console.WriteLine("The sum of purchases is " + purchases.Sum());
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+            Console.WriteLine();
+            Console.WriteLine("The most expensive product in prices is " + prices.Max());
+            Console.WriteLine();
+            // Build a collection of customers who are millionaires
+
+
+            List<Customer> customers = new List<Customer>() {
+                new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
+
+            IEnumerable<Customer> millionairesClub = customers.Where(customer => customer.Balance > 999999);
+            Console.WriteLine("Millionaires Club:");
+            foreach (Customer m in millionairesClub)
+            {
+              Console.WriteLine($"{m.Name}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Banks and # of Customers");
+
+            List<Bank> countOfMillionairesAtEachBank = (from customer in millionairesClub
+              group customer by customer.Bank into millionaires
+              select new Bank {
+                BankName = millionaires.Key,
+                NumberOfCustomers = millionaires.Count()
+              }).OrderByDescending(b => b.NumberOfCustomers).ToList();
+
+              
+            foreach (Bank c in countOfMillionairesAtEachBank)
+            {
+              Console.WriteLine($"{c.BankName} {c.NumberOfCustomers}");
+            }
+
         }
     }
 }
